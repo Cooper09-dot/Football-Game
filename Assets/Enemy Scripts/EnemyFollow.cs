076f522c284bae98b0497928a47fcbad1436d294
Jermaine09 <cooper906@morrisville.edu>
2019-12-09 11:59:21 -0500

    Football Game

---
+++ Assets/Enemy Scripts/EnemyFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public GameObject Player;
+    public float movementSpeed = 4;
+
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+}
